a975ae58072aa30b7c29c385f71ad1fd961750ef
Kristan Kenney <40798553+kristankenney@users.noreply.github.com>
2020-07-24 10:24:28 -0300

    Improvements to upgrade functions
    * Improved backup functionality to back up service configuration files and default template sets
    * Use src/deb/hestia/upgrade.conf to control common items for a build.
    * Add UPGRADE_SEND_EMAIL and UPGRADE_SEND_EMAIL_LOG to hestia.conf and UI - control update notification behaviors
    * Add verbose logging for non-release builds.
    * Add warnings for non-release builds to installer output.
    * Additional notifications for different release types to gather feedback.

--- install/upgrade/versions/latest.sh
+++ install/upgrade/versions/latest.sh
@@ -5,13 +5,3 @@
 #######################################################################################
 #######                      Place additional commands below.                   #######
 #######################################################################################
-
-# Update template files to add warnings
-# Backup current templates
-cp -r -f $HESTIA/data/templates/* $HESTIA_BACKUP/templates/
-echo "[ ! ] Updating default web domain templates..."
-$BIN/v-update-web-templates
-echo "[ ! ] Updating default mail domain templates..."
-$BIN/v-update-mail-templates
-echo "[ ! ] Updating default DNS zone templates..."
-$BIN/v-update-dns-templates
