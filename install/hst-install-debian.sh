1d0c4f298d391f7f779baa32edf431886f6e5da1
Kristan Kenney <40798553+kristankenney@users.noreply.github.com>
2020-07-28 20:19:12 -0300

    Ensure correct version is downloaded from APT

--- install/hst-install-debian.sh
+++ install/hst-install-debian.sh
@@ -42,7 +42,7 @@ if [ "$release" -eq 9 ]; then
         mariadb-client mariadb-common mariadb-server postgresql
         postgresql-contrib phppgadmin phpmyadmin mc flex whois rssh git idn zip
         sudo bc ftp lsof rrdtool quota e2fslibs bsdutils e2fsprogs curl
-        imagemagick fail2ban dnsutils bsdmainutils cron hestia hestia-nginx
+        imagemagick fail2ban dnsutils bsdmainutils cron hestia=${HESTIA_INSTALL_VER} hestia-nginx
         hestia-php expect libmail-dkim-perl unrar-free vim-common acl sysstat
         rsyslog ssh setpriv ipset libapache2-mod-ruid2"
 elif [ "$release" -eq 10 ]; then
@@ -59,7 +59,7 @@ elif [ "$release" -eq 10 ]; then
         mariadb-common mariadb-server postgresql postgresql-contrib phpmyadmin
         phppgadmin mc flex whois git idn zip sudo bc ftp lsof rrdtool
         quota e2fslibs bsdutils e2fsprogs curl imagemagick fail2ban dnsutils
-        bsdmainutils cron hestia hestia-nginx hestia-php expect
+        bsdmainutils cron hestia=${HESTIA_INSTALL_VER} hestia-nginx hestia-php expect
         libmail-dkim-perl unrar-free vim-common acl sysstat rsyslog ssh util-linux
         ipset libapache2-mpm-itk"
 fi
